a5907fdd75bf2f8b5ce55324997c880cb5c20622
Chris Emerson <github@mail.nosreme.org>
2019-01-13 23:03:13 +0000

    WIP on manually adding some components towards making a scene.

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,40 @@ extern crate amethyst;
 
 use amethyst::{
     prelude::*,
-    renderer::{DisplayConfig, DrawFlat, Pipeline, PosNormTex, RenderBundle, Stage},
+    renderer::{PosNormTex, DrawShaded, Projection, Camera, Light},
+    core::{Transform, transform::TransformBundle},
     utils::application_root_dir,
 };
+use nalgebra::geometry::Translation3;
 
 struct Example;
 
-impl<'a, 'b> SimpleState<'a, 'b> for Example {}
+impl SimpleState for Example {
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        let world = data.world;
+
+        let mut camera_trans = Transform::default();
+        camera_trans.set_z(-0.4);
+
+        // Make the camera
+        world
+            .create_entity()
+            .with(Camera::from(Projection::perspective(1.3, 1.0471975512)))
+            .with(camera_trans)
+            .build();
+
+        // Make a light
+        let mut light_transform = Transform::default();
+        light_transform.set_x(2.0);
+        light_transform.set_y(2.0);
+        light_transform.set_z(-2.0);
+        world
+            .create_entity()
+            .with(Light::Point(Default::default()))
+            .with(light_transform)
+            .build();
+    }
+}
 
 fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
@@ -17,16 +44,10 @@ fn main() -> amethyst::Result<()> {
         "{}/resources/display_config.ron",
         application_root_dir()
     );
-    let config = DisplayConfig::load(&path);
 
-    let pipe = Pipeline::build().with_stage(
-        Stage::with_backbuffer()
-            .clear_target([0.00196, 0.23726, 0.21765, 1.0], 1.0)
-            .with_pass(DrawFlat::<PosNormTex>::new()),
-    );
-
-    let game_data =
-        GameDataBuilder::default().with_bundle(RenderBundle::new(pipe, Some(config)))?;
+    let game_data = GameDataBuilder::default()
+        .with_bundle(TransformBundle::new())?//RenderBundle::new(pipe, Some(config)))?
+        .with_basic_renderer(path, DrawShaded::<PosNormTex>::new(), false)?;
     let mut game = Application::new("./", Example, game_data)?;
 
     game.run();
